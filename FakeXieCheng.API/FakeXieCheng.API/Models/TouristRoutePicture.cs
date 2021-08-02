1ffa0723d90cd070d3e78893d3ba4a15871f505e
miayang <yyangzp@163.com>
2021-08-02 08:43:43 +0800

    model

--- FakeXieCheng.API/FakeXieCheng.API/Models/TouristRoutePicture.cs
+++ FakeXieCheng.API/FakeXieCheng.API/Models/TouristRoutePicture.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,12 @@ namespace FakeXieCheng.API.Models
 {
     public class TouristRoutePicture
     {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Url { get; set; }
+        [ForeignKey("TouristRouteId")]
         public Guid TouristRouteId { get; set; }
         public TouristRoute TouristRoute { get; set; }
     }
