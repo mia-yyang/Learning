9bf6d3e7d6ae9e401a40c44c18038bf228b69728
miayang <yyangzp@163.com>
2021-08-02 21:28:16 +0800

    first migration

--- FakeXieCheng.API2/FakeXieCheng.API/FakeXieCheng.API/Models/TouristRoute.cs
+++ FakeXieCheng.API2/FakeXieCheng.API/FakeXieCheng.API/Models/TouristRoute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,18 +9,29 @@ namespace FakeXieCheng.API.Models
 {
     public class TouristRoute
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal OriginalPrice { get; set; }
+        [Range(0.0, 1.0)]
         public double? DiscountPresent { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime? UpdateTime { get; set; }
         public DateTime? DepartureTime { get; set; }
+        [MaxLength]
         public string Features { get; set; }
+        [MaxLength]
         public string Fees { get; set; }
+        [MaxLength]
         public string Notes { get; set; }
-        public int Rating { get; set; }
         public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+            = new List<TouristRoutePicture>();
+        public double? Rating { get; set; }
     }
 }
