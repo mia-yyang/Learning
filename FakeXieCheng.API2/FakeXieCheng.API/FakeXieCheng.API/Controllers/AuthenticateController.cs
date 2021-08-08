6e2de0707fd621c24c5121d8b1fa41408afee7d0
miayang <yyangzp@163.com>
2021-08-08 16:08:00 +0800

    jwt模拟用户登录

---
+++ FakeXieCheng.API2/FakeXieCheng.API/FakeXieCheng.API/Controllers/AuthenticateController.cs
@@ -0,0 +1,59 @@
+﻿using FakeXieCheng.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeXieCheng.API.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            // 1.验证用户名密码
+
+            // 2.创建jwt
+            // header
+            var signingAlgorithm = SecurityAlgorithms.HmacSha256;
+            // payload
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "fake_user_id")
+            };
+            // signiture
+            var secretByte = Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]);
+            var signingKey = new SymmetricSecurityKey(secretByte);
+            var signingCredentials = new SigningCredentials(signingKey, signingAlgorithm);
+            // 3.return 200 ok+jwt
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"],
+                claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials
+            );
+
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(tokenStr);
+        }
+    }
+}
